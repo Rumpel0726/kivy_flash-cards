1add44fe3513c163eefd1b16a82dfa24e3d6a61a
Rumpel0726 <126333063+Rumpel0726@users.noreply.github.com>
2023-03-04 12:29:01 +0300

    Make some changes

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("123")
+from kivy.app import App
+from kivy.uix.button import Button
+
+class MyApp(App):
+    def build(self):
+        return Button(text="Старт")
+
+if __name__ == "__main__":
+    MyApp().run()
